85c358275b41155c64023f322707999fcb920be5
Paulo Martins <phmartins6@gmail.com>
2013-02-14 14:19:19 -0200

    Changes

--- products/models.py
+++ products/models.py
@@ -1,19 +1,14 @@
 from django.db import models
+from users.models import User
 
 class Type(models.Model):
 	name = models.CharField(max_length=200)
 
-class User(models.Model):
-	name = models.CharField(max_length=200)
-	last_name = models.CharField(max_length=200)
-	email = models.CharField(max_length=300)
-	password = models.CharField(max_length=200)
-
-class Product(models.Model):
+class Item(models.Model):
 	name = models.CharField(max_length=200)
 	obs = models.CharField(max_length=200)
 	date = models.DateField()
 	end_date = models.DateField()
 	value = models.FloatField()
 	type = models.ForeignKey(Type)
-	user = models.ForeignKey(User)
+	user = models.ManyToManyField(User)
---
+++ users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class User(models.Model):
+	name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+	email = models.CharField(max_length=300)
+	password = models.CharField(max_length=200)
